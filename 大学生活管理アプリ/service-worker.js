13082d84ac378fb4a130f1e9d3d5becd1cb11917
yuusep16-tech <yuusep16@gmail.com>
2026-06-02 22:02:00 +0900

    Add PWA app files

---
+++ 大学生活管理アプリ/service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "assignment-calendar-pwa-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
+        .then(response => {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, responseCopy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
