447f7d5781b1a75cbfd2a38c3fbc645db3394de1
yuusep16-tech <yuusep16@gmail.com>
2026-06-06 01:40:12 +0900

    cssの分離

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = "assignment-app-v14";
+const CACHE_NAME = "assignment-app-v15";
 const APP_SHELL = [
   "./",
   "./index.html",
+  "./style.css",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
